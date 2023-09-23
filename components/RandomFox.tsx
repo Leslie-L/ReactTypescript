7813725b5e054b795235e3902d03176ef397c3d2
Leslie-L <leslie.centeno@galileo.edu>
2023-09-22 19:16:57 -0600

    adding images

---
+++ components/RandomFox.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react"
+import gray from '@/Images/Solid_grey.svg'
+type Props = {image:string}
+
+export const RandomFox = ({image}:Props):JSX.Element =>{
+    const node = useRef<HTMLImageElement>(null)
+    const [src,setSrc]= useState("https://upload.wikimedia.org/wikipedia/commons/8/86/Solid_grey.svg");
+
+    useEffect(()=>{
+        //nuevo observador
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach(entry=>{
+                if(entry.isIntersecting){
+                   setSrc(image)
+                }
+            })
+        })
+        //observe node
+        if(node.current)
+            observer.observe(node.current)
+        //desconectar
+        return ()=>{
+            observer.disconnect()
+        }
+    },[image])
+     
+
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img ref={node} src={src} width={300} className='rounded-md shadow-md shadow-blue-100' alt='new fox image'/>
+}
