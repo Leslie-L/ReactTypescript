da7c5824a9866d96301fc3257ae362237852d7c0
Leslie-L <leslie.centeno@galileo.edu>
2023-09-25 22:23:55 -0600

    types uuidv4

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -22,16 +22,22 @@ export default function Page() {
     setImages([...images,newItem]);
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>hello world!</h1>
-        <button className='w-20 h-10 bg-blue-500' onClick={addNewFox}>Add Fox</button>
-        {images.map((item)=>{
-          return(
-            <div key={item.id} className='p-4'>
-              <RandomFox image={item.url}/>
+    <main className="flex min-h-screen flex-col items-center">
+        <h1 className='font-bold text-orange-700  text-4xl text-center font-sans my-3'>Galería</h1>
+        <button className=' w-36 h-10 bg-lightBrown rounded-sm text-white hover:bg-cream my-3' onClick={addNewFox}>Ver más Fotos</button>
+        <section className='w-full flex flex-col justify-center items-center'>
+            <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2'>
+            <img width={300} className='rounded-md shadow-md shadow-blue-100' src="https://images.pexels.com/photos/208976/pexels-photo-208976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="zorro" />
+            {images.map((item)=>{
+              return(
+                <div key={item.id} className='p-4'>
+                  <RandomFox image={item.url}/>
+                </div>
+              )
+            })}
             </div>
-          )
-        })}
+        </section>
+        
     </main>
   )
 }
